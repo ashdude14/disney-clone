b6dd180e4009053a5d1b7aa59abdd5dd9cee4957
Ashish Kumar <ashish.kumar.singh.jee@gmail.com>
2023-12-30 23:44:03 +0530

    toggle setting &dotvertical coponents

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import logo from './../assets/Images/logo.png'
 import { HiHome,
     HiMagnifyingGlass,
     HiStar,
@@ -9,6 +8,7 @@ import { HiPlus,HiDotsVertical } from "react-icons/hi";
 import logo1 from "./../assets/Images/logo.png"
 import Headeritem from './Headeritem';
 function Header() {
+    const [toggle,setToggel]=useState(false);
     const menu=[
         {
             name:'HOME',
@@ -40,17 +40,24 @@ function Header() {
     <div className='flex gap-8 items-center' >
     <img src={logo1} className='w-[80px] 
     md:w-[110px] object-cover' alt="disney+" />
-    <div className='flex md:hidden gap-8'>
+    <div className='flex md:hidden gap-5'>
     {
         menu.map((item,index)=>index<3&&(
-            <Headeritem name={item.name} Icon={item.icon}/>
+            <Headeritem name={""} Icon={item.icon}/>
         ))
     }
-     <div className='md:hidden'>       
+
+    <div className='md:hidden' onClick={()=>setToggel(!toggle)}>       
             <Headeritem name={''} Icon={HiDotsVertical} />
+
+            {toggle&&<div className='absolute mt-3 bg-[#121212] border-[1px] p-3 border-gray-700 px-5 py-4   '>
+            {menu.map((item,index)=>index>2&&(
+            <Headeritem name={item.name} Icon={item.icon}/>
+        ))}
+            </div>}
     </div>
     </div>
-    <div className='hidden md:flex gap-8'>
+    <div className='hidden md:flex gap-5'>
     {
         menu.map((item)=>(
             <Headeritem name={item.name} Icon={item.icon}/>
--- src/Componets/Headeritem.jsx
+++ src/Componets/Headeritem.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Headeritem({name,Icon}) {
   return (
-    <div className='text-white flex text-center gap-3 text-[15px] font-semibold hover:underline underline-offset-8'>
+    <div className='text-white flex text-center gap-3 text-[15px] font-semibold hover:underline underline-offset-8 mb-3'>
       <Icon/>
-      <h2 className='hidden md:block'>{name} </h2>
+      <h2>{name}</h2>
+      <h2 className='hidden md:block'>{""} </h2>
     </div>
   )
 }
