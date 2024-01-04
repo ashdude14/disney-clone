e5e609c7b02c131018fd06f0f13da40e8b109923
Ashish Kumar <ashish.kumar.singh.jee@gmail.com>
2024-01-04 07:46:19 +0530

    Horizontal Movie Card Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ import './App.css'
 import Header from './Componets/Header'
 import Slider from './Componets/Slider'
 import ProductionHouse from './Componets/ProductionHouse'
+import GenreMovieList from './Componets/GenreMovieList'
+
+
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <div className="">
         <Header/>
+        
         <Slider/>
+
         <ProductionHouse/>
+
+        <GenreMovieList/>
     </div>
   )
 }
 
-export default App
+export default App 
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,11 +1,17 @@
-import axios from 'axios';
-const movieBaseUrl="https://api.themoviedb.org/3"
-const apiKey="af5fce3f491f9c138bf5547028bd9997";
-const Url="https://api.themoviedb.org/3/trending/movie/day?api_key="
-const getTrendingVideos=axios.get(Url+apiKey);
-console.log(Url+apiKey);
+import axios from "axios";
 
-export default {
-    getTrendingVideos   
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='2ec0d66f5bdf1dd12eefa0723f1479cf'
+
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+const getTrendingVideos=axios.get(movieBaseUrl+
+    "/trending/all/day?api_key="+api_key);
+    const getMovieByGenreId=(id)=>
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
+
+export default{
+    getTrendingVideos,
+    getMovieByGenreId
 }
-//https://api.themoviedb.org/3/trending/movie/day?api_key=af5fce3f491f9c138bf5547028bd9997
